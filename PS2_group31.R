39437e395e0d393180bf1a6568ac1f55aa570053
cagdasyetkin <cagdasyetkin@yahoo.com>
2017-10-12 07:38:13 +0200

    after price_online NAs

--- PS2_group31.R
+++ PS2_group31.R
@@ -1,16 +1,32 @@
+rm(list=ls())
 amazon <- read.csv("amazon_compare.csv")
 
 str(amazon)
 summary(amazon)
 
+
+#price correlation
+
 amazon2 <- amazon[which(!is.na(amazon$price) & !is.na(amazon$price_online)),]
 summary(amazon2)
 
-
 install.packages("GGally")
 library(GGally)
+
 priceset <- data.frame(amazon2$price, amazon2$price_online, amazon2$price_amazon)
 priceset2 <- priceset[which((priceset$amazon2.price < 2000) & (priceset$amazon2.price_amazon < 2000)),]
 summary(priceset2)
 
-ggpairs(priceset)
+ggpairs(priceset2)
+
+#missing observations in the original dataset
+
+summary(amazon)
+str(amazon)
+
+#missing values by category like retailer
+library(data.table)
+amazon3 <- data.table(amazon) 
+table(is.na(amazon3$price_online), amazon3$category)
+table(is.na(amazon3$price_online), amazon3$retailer_id)
+
