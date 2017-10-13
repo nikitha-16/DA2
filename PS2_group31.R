24d7dcb4c70fa071df44c3d90b0ae42f46f0f702
cagdasyetkin <cagdasyetkin@yahoo.com>
2017-10-13 07:58:09 +0200

    NA replacement start

--- PS2_group31.R
+++ PS2_group31.R
@@ -30,3 +30,24 @@ amazon3 <- data.table(amazon)
 table(is.na(amazon3$price_online), amazon3$category)
 table(is.na(amazon3$price_online), amazon3$retailer_id)
 
+median <- median(amazon3$price_amazon)
+
+amazon4 <- amazon3[, Amz_pricecat := cut(price_amazon, c(0,
+                                                     median,
+                                                     Inf),
+                         labels = c('low', 'high')),]
+
+table(is.na(amazon4$price_online), amazon4$Amz_pricecat)
+
+#replace missing values in price_online with its mean
+
+mean <- mean(amazon4$price_online, na.rm = TRUE)
+amazon4$price_online[which(is.na(amazon4$price_online))] <- mean
+summary(amazon4$price_online)
+
+#replace missing values in price_online with category means like retailer or product
+
+retailer_prc_avg <- amazon4[, .(retailer_prc_avg = mean(price_online, na.rm = TRUE)), by = retailer_id]
+
+
+
