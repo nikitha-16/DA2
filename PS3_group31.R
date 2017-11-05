e1d9a2a9df55994d23fa3595733fd20043ecac80
Peter Paziczki <peter.paziczki@gmail.com>
2017-11-05 11:29:55 +0100

    Peter committing Q8 - Q15

--- PS3_group31.R
+++ PS3_group31.R
@@ -1,5 +1,6 @@
 #Problem Set 3
 rm(list=ls())
+library(data.table)
 # 1.	Use the amazon_compare.csv file and load it into R.
 amazon <- read.csv("amazon_compare.csv")
 
@@ -18,7 +19,7 @@ set.seed(31)
 amazon3$id_rand <- rnorm(nrow(amazon3), mean = 0, sd = 1)
 amazon4 <- amazon3[order(amazon3$id_rand, decreasing = FALSE),]
 amazon4 <- amazon4[1:1000,]
-#This is simple random samlping
+#This is simple random samlping.
 
 # 3.	Copy-paste your answer in Q4 in PS1.
 mean(amazon3$price)
@@ -70,4 +71,119 @@ ggplot(amazon5, aes(price_online)) + geom_histogram() + facet_wrap(~missing_onli
 ggplot(amazon5, aes(missing_online, price)) + geom_boxplot()
 ggplot(amazon5, aes(missing_online, price_online)) + geom_boxplot()
 
-#End of coding, you can go ahead with question 8
+#8. Compare the means of the variables price and price_amazon variables across the two groups of the missing_online.
+
+amazon5 <- data.table(amazon5) # creating a data.table object
+amazon5[, lapply(.SD, mean), by = missing_online, .SDcols = c("price", "price_amazon")] # computing the means of
+# price and price_amazon grouped by missing_online (dummy variable with 0s and 1s)
+
+#9.	Compare the means of the variables price and  price_amazon and for each of the different retailers (or good categories).
+# For which retailers (or good categories) you see no differences in the means of price and price_amazon?
+# For which retailers (or good categories) do you see a difference in means the prices and price_amazon?
+
+str(amazon5)
+amazon5[, lapply(.SD, mean), by = category, .SDcols = c("price", "price_amazon")]
+amazon5[, lapply(.SD, mean), by = retailer_s, .SDcols = c("price", "price_amazon")]
+amazon5[, lapply(.SD, mean), by = list(category, retailer_s), .SDcols = c("price", "price_amazon")]
+
+#10. Due to the knowledge from PS2_part2 and the above evidence, can you say anything wether the observations
+# missing in the online price are missing at random?
+
+# Please find our answer in the pdf.
+
+amazon6 <- amazon[is.na(amazon$price_online),]
+amazon6 <- data.table(amazon6)
+amazon6[, lapply(.SD, mean), by = category, .SDcols = c("price", "price_amazon")]
+amazon6[, lapply(.SD, mean), by = retailer_s, .SDcols = c("price", "price_amazon")]
+amazon6[, lapply(.SD, mean), by = list(category, retailer_s), .SDcols = c("price", "price_amazon")]
+
+amazon7 <- amazon6[which(amazon6$price <= quantile(amazon6$price, prob = 0.95)),]
+amazon7 <- data.table(amazon7)
+amazon7[, lapply(.SD, mean), by = category, .SDcols = c("price", "price_amazon")]
+amazon7[, lapply(.SD, mean), by = retailer_s, .SDcols = c("price", "price_amazon")]
+amazon7[, lapply(.SD, mean), by = list(category, retailer_s), .SDcols = c("price", "price_amazon")]
+
+#11. Do the same procedure as 10 in PS2_part2. What type of sampling is this?
+
+set.seed(31)
+amazon5$id_rand <- runif(nrow(amazon5), min = 0, max = 1)
+
+#12.	Sort id_rand and select the first 100 observations in your dataset. Construct a 95 percent confidence interval (CI)
+# for the mean of price. Interpret this confidence interval. What does a narrower confidence interval mean? Is the mean
+# of price you got in 6 included in this interval? Should it be included?
+
+# sorting by id_rand
+amazon5 <- amazon5[order(amazon5$id_rand),]
+
+# selecting the first hundred observation and computing mean and standard deviation
+a <- 1
+b <- 100
+sample_size <- b-a+1
+sd_dev <- amazon5[a:b, sd(price)]
+mean <- amazon5[a:b, mean(price)]
+CI_left <- mean-qnorm(0.975)*sd_dev/sqrt(sample_size)
+CI_right <- mean+qnorm(0.975)*sd_dev/sqrt(sample_size)
+
+Q12 <- c(sample_size, mean, sd_dev, CI_left, CI_right)
+
+#13.	Repeat 11, sort id_rand and select and select the first 200 observations in your dataset make summary statistics for price.
+#Construct a 95 percent (CI) for the mean of price. How does this confidence interval compare to the one in 12?
+#Does this CI include the mean of price in 6? (Non mandatory!)
+
+a <- 1
+b <- 200
+sample_size <- b-a+1
+sd_dev <- amazon5[a:b, sd(price)]
+mean <- amazon5[a:b, mean(price)]
+CI_left <- mean-qnorm(0.975)*sd_dev/sqrt(sample_size)
+CI_right <- mean+qnorm(0.975)*sd_dev/sqrt(sample_size)
+
+Q13 <- c(sample_size, mean, sd_dev, CI_left, CI_right)
+
+#14.	Repeat 11, sort id_rand and select and select the first and select the first 300 observations in your dataset make
+#summary statistics for price. Construct a 95 percent (CI) for the mean of price. How does this confidence interval compare
+# to the one in12 and 13? Does this CI include the mean of price in 6? (Non mandatory!)
+
+a <- 1
+b <- 300
+sample_size <- b-a+1
+sd_dev <- amazon5[a:b, sd(price)]
+mean <- amazon5[a:b, mean(price)]
+CI_left <- mean-qnorm(0.975)*sd_dev/sqrt(sample_size)
+CI_right <- mean+qnorm(0.975)*sd_dev/sqrt(sample_size)
+
+Q14 <- c(sample_size, mean, sd_dev, CI_left, CI_right)
+
+
+#15.	Repeat 11, sort id_rand and select and select the first and select the first 1000 observations in your dataset make summary
+# statistics for price. Construct a 95 percent (CI) for the mean of price. How does this confidence interval compare to the one
+# in 12, 13 and 14? Does this CI include the mean of price in 6?
+
+a <- 1
+b <- 1000
+sample_size <- b-a+1
+sd_dev <- amazon5[a:b, sd(price)]
+mean <- amazon5[a:b, mean(price)]
+CI_left <- mean-qnorm(0.975)*sd_dev/sqrt(sample_size)
+CI_right <- mean+qnorm(0.975)*sd_dev/sqrt(sample_size)
+
+Q15 <- c(sample_size, mean, sd_dev, CI_left, CI_right)
+
+CI_matrix <- matrix(c(Q12, Q13, Q14, Q15), nrow = 4, byrow = TRUE, dimnames = list(c("Q12", "Q13", "Q14", "Q15"), c("sample size", "sample mean", "sample st dev", "interval_left", "interval_right")))
+CI_matrix
+
+#16.	Add 1 as the last digit in your seed, example if your group number is 106 your seed should be set as 1061 and repeat 12-15.
+# Have you answers changed? (Non mandatory for part-time students!)
+
+
+
+#17.	In your opinion how would your answers in points 12-15 change, if instead of using 95 percent (CI) you use a 90 or 99 percent (CI)?
+
+
+
+#18.	Many times in Data Analysis we need to generate new variables from the existing dataset as well as transform the existing ones.
+# Create a new variable which takes on the number of times each date (“date” variable) is found in the dataset. Call this variable
+# times_date. What type of variable is this? Do the following transformations:
+# normalization (times_date-min(times_date)/max(times_date)-min(times_date)), standardization (times_date-mean(times_date)/sd(times_date))
+# and logarithmic (log(times_date)). What can you say about these transformations?
+
